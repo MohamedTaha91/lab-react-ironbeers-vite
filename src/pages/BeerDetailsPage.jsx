45735365259e8b523a678b04954e07bf986c589a
Mohamed Taha <Taha_Acme@hotmail.com>
2023-11-27 10:34:36 +0100

    lab done

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,3 +1,39 @@
-function BeerDetailsPage() {}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {Link} from "react-router-dom"
+import axios from "axios"
+
+const BeerDetailsPage = () => {
+    const [beer, setBeer] = useState(null);
+    const { beerId } = useParams();
+
+    useEffect(() => {
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`)
+            .then((response) => {
+                setBeer(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching beer details:", error);
+            });
+    }, [beerId]);
+
+    return (
+        <div>
+            {beer ? (
+                <div>
+                    <img src={beer.image_url} alt={beer.name} />
+                    <h2>{beer.name}</h2>
+                    <p>{beer.tagline}</p>
+                    <p>First Brewed: {beer.first_brewed}</p>
+                    <p>Attenuation Level: {beer.attenuation_level}</p>
+                    <p>Description: {beer.description}</p>
+                    <p>Contributed by: {beer.contributed_by}</p>
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+};
 
 export default BeerDetailsPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,22 +3,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
+
 function HomePage() {
     return (
         
         <div className="homepage-container">
             <div beer-container>
-                <img className="beers" src="src\assets\beers.png" />
+                <img className="beers" src="..\src\assets\beers.png" />
                 <Link id="beers" to="/beers"> All Beers</Link>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div>
-                <img className= "random-beer" src="src\assets\random-beer.png" />
+                <img className= "random-beer" src="..\src\assets\random-beer.png" />
                 <Link id="random-beer" to="/random-beer"> Random Beer</Link>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div>
-                <img className="new-beer" src="src\assets\new-beer.png" />
+                <img className="new-beer" src="..\src\assets\new-beer.png" />
                 <Link id="new-beer" to="/new-beers"> New Beer</Link>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
